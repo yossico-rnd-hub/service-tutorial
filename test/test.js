8eb1831de7fc3ca7ef936e0f877513c8d7899fc8
yossi <yossico@rnd-hub.com>
2020-01-12 19:32:51 +0200

    ref

--- test/test.js
+++ test/test.js
@@ -9,10 +9,10 @@ var redisLocker = new RedisLocker({
     host: 'localhost'
 });
 
-async function test() {
-    var resourceId = 'lilo';
-    var ttl = 10;
+var resourceId = 'lilo';
+var ttl = 10;
 
+async function test_async_await() {
     try {
         let token = await redisLocker.lock(resourceId, ttl);
         console.log('lock success: ' + token);
@@ -29,4 +29,51 @@ async function test() {
     console.log('done.')
 }
 
-test();
+function test_promise() {
+    redisLocker.lock(resourceId, ttl)
+        .then((token) => { console.log('locked: ' + token) })
+        .then((token) => { return redisLocker.unlock(resourceId, token) })
+        .then((res) => { console.log('unlocked success.') })
+        .then(() => { return redisLocker.unlock(resourceId, token) })
+        .catch((res) => { console.log('unlocked again failure!'); resolve(); })
+        .then(() => { return redisLocker.unlock(resourceId, 'fake-token') })
+        .catch((res) => { console.log('unlocked fake-token failure!') })
+}
+
+// function test_callback() {
+//     redisLocker.lock(resourceId, ttl, function (err, result) {
+//         if (err) {
+//             console.log('error: ' + err);
+//         }
+//         else {
+//             let token = result;
+//             console.log('locked: ' + token);
+
+//             redisLocker.unlock(resourceId, token, function (err, result) {
+//                 if (0 == result)
+//                     console.log('unlocked failed! ' + token)
+//                 else
+//                     console.log('unlocked succeeded: ' + token)
+//             });
+
+//             redisLocker.unlock(resourceId, token, function (err, result) {
+//                 if (0 == result)
+//                     console.log('unlock again failed! ' + token)
+//                 else
+//                     console.log('unlock again succeeded: ' + token)
+//             });
+
+//             redisLocker.unlock(resourceId, 'fake-token', function (err, result) {
+//                 token = result;
+//                 if (0 == result)
+//                     console.log('unlocked fake-token failed!')
+//                 else
+//                     console.log('unlocked fake-token succeeded.')
+//             });
+//         }
+//     });
+// }
+
+// test_async_await();
+test_promise();
+// test_callback();
